d968600b46a76015b8f050f10cb945541ad81dbf
Michael Gerhold <michael.gerhold@web.de>
2020-07-15 19:44:09 +0200

    Included ImGui

--- src/Chip8Renderer/Chip8Renderer.cpp
+++ src/Chip8Renderer/Chip8Renderer.cpp
@@ -1,8 +1,22 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
 #include <iostream>
 
-#include <Chip8Renderer/Chip8Renderer.hpp>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#ifdef _MSC_VER
+    // suppress warnings for ImGui headers
+    #pragma warning( push , 0 )
+    #pragma warning( disable : 26477 26455 26440 26493 26477 26432 26447 26481 26496 26485 26495 )
+    #pragma warning( disable : ALL_CODE_ANALYSIS_WARNINGS )
+#endif
+#include <imgui.h>
+#include <examples/imgui_impl_glfw.h>
+#include <examples/imgui_impl_opengl3.h>
+#ifdef _MSC_VER
+    #pragma warning( pop )
+#endif
+
+#include "Chip8Renderer/Chip8Renderer.hpp"
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) noexcept;
 void processInput(GLFWwindow* window) noexcept;
@@ -15,7 +29,7 @@ Chip8Renderer::Chip8Renderer() noexcept {}
 
 bool Chip8Renderer::createWindow() {
     // glfw: initialize and configure
-    // ------------------------------
+    // ------------------------------    
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -43,11 +57,21 @@ bool Chip8Renderer::createWindow() {
         return false;
     }
 
+    // init ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    const ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    constexpr char const * glsl_version = "#version 330";
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
     // render loop
     // -----------
     while (!glfwWindowShouldClose(window)) {
         // input
         // -----
+        glfwPollEvents();
         processInput(window);
 
         // render
@@ -55,16 +79,31 @@ bool Chip8Renderer::createWindow() {
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        // ImGui test
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        bool opened = true;
+        ImGui::ShowDemoWindow(&opened);
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
+
+    // Cleanup ImGui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     // ------------------------------------------------------------------
+    glfwDestroyWindow(window);
     glfwTerminate();
-    return true;
+
+    return 0;
 }
 
 void processInput(GLFWwindow* window) noexcept {
@@ -74,7 +113,7 @@ void processInput(GLFWwindow* window) noexcept {
 
 // glfw: whenever the window size changed (by OS or user resize) this callback function executes
 // ---------------------------------------------------------------------------------------------
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) noexcept {
+void framebuffer_size_callback(GLFWwindow*, int width, int height) noexcept {
     // make sure the viewport matches the new window dimensions; note that width and 
     // height will be significantly larger than specified on retina displays.
     glViewport(0, 0, width, height);
